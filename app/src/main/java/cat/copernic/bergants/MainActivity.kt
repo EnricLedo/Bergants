78a0021b911a7d87d03307c99ffc49690495d074
= <=>
2022-10-27 12:12:29 +0200

    RecycleView de Notícies

--- app/src/main/java/cat/copernic/bergants/MainActivity.kt
+++ app/src/main/java/cat/copernic/bergants/MainActivity.kt
@@ -2,13 +2,21 @@ package cat.copernic.bergants
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import cat.copernic.bergants.adapter.NoticiaAdapter
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerNoticies)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = NoticiaAdapter(NoticiaProvider.noticiaList)
     }
 } 
