9babf042f03b2150ea78d9c8530be9d5e46e80a4
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-11-02 08:53:10 +0100

    Canvi nom afegir actuació

--- app/src/main/java/cat/copernic/bergants/AfegirActuacio.kt
+++ app/src/main/java/cat/copernic/bergants/AfegirActuacio.kt
@@ -34,7 +34,7 @@ class AfegirActuacioSiAutocar : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_afegir_actuacio_si_autocar, container, false)
+        return inflater.inflate(R.layout.fragment_afegir_actuacio, container, false)
     }
 
     companion object {
