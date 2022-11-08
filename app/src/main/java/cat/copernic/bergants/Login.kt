a6e7a8faba7382f16a17e9848008e17ac410080d
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-11-08 11:42:21 +0100

    Login amb binding

--- app/src/main/java/cat/copernic/bergants/Login.kt
+++ app/src/main/java/cat/copernic/bergants/Login.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import cat.copernic.bergants.databinding.ActivityLoginBinding
+import cat.copernic.bergants.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -22,11 +24,8 @@ class Login : AppCompatActivity() {
       És recomanable per seguretat i facilitar-nos la feina que els noms d'aquests atributs siguin els mateixos que els noms dels
       id dels components del fitxer xml*/
 
-    private lateinit var correuLogin:EditText
-    private lateinit var contrasenyaLogin:EditText
-    private lateinit var botoLogin:Button
-    private lateinit var textRecuperarContrasenya: TextView
     private lateinit var loginpage: View
+    private lateinit var binding: ActivityLoginBinding
 
     //Declarem un atribut de tipus FirebaseAuth
     private lateinit var auth: FirebaseAuth
@@ -38,10 +37,7 @@ class Login : AppCompatActivity() {
         val user = FirebaseAuth.getInstance().currentUser
 
         //Inicalitzem els atributs amb els components corresponents a l'id passat per paràmetre
-        correuLogin = findViewById(R.id.email)
-        contrasenyaLogin = findViewById(R.id.password)
-        botoLogin = findViewById(R.id.logIn)
-        textRecuperarContrasenya = findViewById(R.id.forgottenPassword)
+
         loginpage = findViewById(android.R.id.content)
 
 
@@ -50,11 +46,11 @@ class Login : AppCompatActivity() {
 
         //Implementem els listeners per quan l'usuari cliqui un dels botons
 
-        botoLogin.setOnClickListener {
+        binding.logIn.setOnClickListener {
 
             //Guardem les dades introduïdes per l'usuari en el formulari mitjançant text i les transformem amb un String (toString())
-            val correu = correuLogin.text.toString()
-            val contrasenya = contrasenyaLogin.text.toString()
+            val correu = binding.email.text.toString()
+            val contrasenya = binding.password.text.toString()
 
             //Comprovem que els camps no estan buit
             if(correu.isNotEmpty()&&contrasenya.isNotEmpty()){
@@ -65,7 +61,7 @@ class Login : AppCompatActivity() {
                 Snackbar.make(it,"Introdueix un correu i una contrasenya", Snackbar.LENGTH_LONG).show()
             }
         }
-        textRecuperarContrasenya.setOnClickListener {
+        binding.forgottenPassword.setOnClickListener {
             startActivity(Intent(this,RecuperarContrasenya::class.java))
             finish() //Alliberem memòria un cop finalitzada aquesta tasca.
         }
