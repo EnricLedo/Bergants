6edc6b9c9b7fb0d86f4ea9d8d3d45ca77b973855
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-11-08 09:24:36 +0100

    Borrada de codi del Main Activity xk petava

--- app/src/main/java/cat/copernic/bergants/MainActivity.kt
+++ app/src/main/java/cat/copernic/bergants/MainActivity.kt
@@ -2,56 +2,23 @@ package cat.copernic.bergants
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.bergants.adapter.NoticiaAdapter
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var toolbar : Toolbar
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        toolbar = findViewById(R.id.myToolbar)
-        setSupportActionBar(toolbar)
-
-        drawerLayout = findViewById(R.id.drawer)
-        navigationView = findViewById(R.id.navigationView)
-
-        navController = findNavController(R.id.fragmentContainerView)
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.perfil_fragment, R.id.configuracio_fragment), drawerLayout)
-        setupActionBarWithNavController(navController, drawerLayout)
-        navigationView.setupWithNavController(navController)
-
-        //initRecyclerView()
+        initRecyclerView()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragmentContainerView)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    /*
     private fun initRecyclerView(){
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerNoticies)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = NoticiaAdapter(NoticiaProvider.noticiaList)
     }
-     */
 }
+
