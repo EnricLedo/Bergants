fe8cca7b02d7b48ab4d706b30b699077544a541c
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2022-11-08 14:20:27 +0100

    hideactionbar

--- app/src/main/java/cat/copernic/bergants/MainActivity.kt
+++ app/src/main/java/cat/copernic/bergants/MainActivity.kt
@@ -3,18 +3,32 @@ package cat.copernic.bergants
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.bergants.adapter.NoticiaAdapter
+import cat.copernic.bergants.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     //Atribut de tipus Button per tancar la sessió
-    private lateinit var botoLogout: Button
+    //private lateinit var botoLogout: Button
 
     //Declarem un atribut de tipus FirebaseAuth
     private lateinit var auth: FirebaseAuth
@@ -22,7 +36,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
+        navController =navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
         /**
         //initRecyclerView()
         //Inicialitzem l'atribut botologout amb el component de l'XML corresponent
@@ -42,6 +64,10 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
          */
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     /**
