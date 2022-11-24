2e7ab2fb8a3ab23c54424b92a555bc9d15790c87
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2022-11-24 12:52:17 +0100

    afegir membre completat i creant afegir bus

--- app/src/main/java/cat/copernic/bergants/afegirMembre.kt
+++ app/src/main/java/cat/copernic/bergants/afegirMembre.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
 import cat.copernic.bergants.databinding.FragmentAfegirMembreBinding
-import cat.copernic.bergants.model.ActuacioModel
+import cat.copernic.bergants.model.MembreModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
 
 class afegirMembre : Fragment() {
     private lateinit var binding: FragmentAfegirMembreBinding
@@ -18,7 +20,7 @@ class afegirMembre : Fragment() {
     private lateinit var nomMembre: EditText
     private lateinit var malnom: EditText
     private lateinit var alcadaEspatlles: EditText
-    private lateinit var AlcadaMans: EditText
+    private lateinit var alcadaMans: EditText
     private lateinit var correuMembre: EditText
     private lateinit var adrecaMembre: EditText
     private lateinit var telefonMembre: EditText
@@ -27,16 +29,78 @@ class afegirMembre : Fragment() {
     //Atribut de tipus Button per afegir un nou membre
     private lateinit var botoAfegir: Button
 
-    //Declarem els atributs on guardarem les actuacions
-    private lateinit var actuacions: ActuacioModel
+    //Declarem els atributs on guardarem els membres
+    private lateinit var membres: MembreModel
+
+    //Declarem i incialitzem un atribut de tipus FirebaseFirestore, classe on trobarem els mètodes per treballar amb la base de dades Firestore
+    private var bd =
+        FirebaseFirestore.getInstance() //Inicialitzem mitjançant el mètode getInstance() de FirebaseFirestore
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        binding = FragmentAfegirMembreBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    fun llegirDades(): MembreModel {
+        //Guardem les dades introduïdes per l'usuari
+        var nomMembre = nomMembre.text.toString()
+        var malnom = malnom.text.toString()
+        var alcadaEspatlles = alcadaEspatlles.text.toString()
+        var alcadaMans = alcadaMans.text.toString()
+        var correuMembre = correuMembre.text.toString()
+        var adrecaMembre = adrecaMembre.text.toString()
+        var telefonMembre = telefonMembre.text.toString()
+        var rolMembre = rolMembre.text.toString()
+
+        return MembreModel(nomMembre, malnom, alcadaEspatlles, alcadaMans, correuMembre,
+            adrecaMembre, telefonMembre, rolMembre)
+    }
+
+    fun afegirMembre(membre: MembreModel) {
+        //Seleccionem la col.lecció on volem afegir el Membre mitjançant la funció collection("Membres"), si no existeix la col.lecció
+        //es crearà, si no la sobreescriurà. Afegim el membre a la col.lecció seleccionada amb un id que genera automàticament Firestore
+        // mitjançant la funció add(membre). Si el membre existeix, es sobreescriurà, sinó es crearà de nou.
+        bd.collection("Membres").add(membre)
+            .addOnSuccessListener { //S'ha afegit el membre...
+                Toast.makeText(
+                    requireActivity(),
+                    "El membre s'ha afegit correctament",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireActivity(), "El membre no s'ha afegit", Toast.LENGTH_LONG)
+                    .show()
+            }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnSaveMem = requireView().findViewById<Button>(R.id.botoGuardarMembre)
+        nomMembre = binding.nomMembre
+        malnom = binding.malnomMembre
+        alcadaEspatlles = binding.espatllesMembre
+        alcadaMans = binding.mansMembre
+        correuMembre = binding.correuMembre
+        adrecaMembre = binding.adrecaMembre
+        telefonMembre = binding.telefonMembre
+        rolMembre = binding.rolMembre
+        botoAfegir = binding.botoGuardarMembre
 
-        btnSaveMem.setOnClickListener {
-            findNavController().navigate(R.id.action_afegirMembre_to_membres_fragment)
+        botoAfegir.setOnClickListener {
+            llegirDades()
+            var membre = llegirDades() //Membre introduit per l'usuari
+            if (membre.nomMembre?.isNotEmpty() == true && membre.malnom?.isNotEmpty() == true && membre.alcadaEspatlles?.isNotEmpty() == true
+                && membre.alcadaMans?.isNotEmpty() == true && membre.correuMembre?.isNotEmpty() == true
+                && membre.adrecaMembre?.isNotEmpty() == true && membre.telefonMembre?.isNotEmpty() == true
+                && membre.rolMembre?.isNotEmpty() == true ) {
+                afegirMembre(membre)
+            } else {
+                Snackbar.make(it, "Falta indroduir parametres!!!", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 }
