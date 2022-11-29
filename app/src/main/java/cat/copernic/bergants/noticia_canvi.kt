fa10627788cdcb4487e3094147a0365d9c0a36b2
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2022-11-29 23:43:50 +0100

    mostrarNoticies

--- app/src/main/java/cat/copernic/bergants/noticia_canvi.kt
+++ app/src/main/java/cat/copernic/bergants/noticia_canvi.kt
@@ -6,11 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.view.plusAssign
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import cat.copernic.bergants.databinding.FragmentNoticiaCanviBinding
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import cat.copernic.bergants.adapter.NoticiaRecyclerAdapter
 import cat.copernic.bergants.model.NoticiaModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class noticia_canvi : Fragment() {
@@ -18,6 +28,10 @@ class noticia_canvi : Fragment() {
     private lateinit var binding: FragmentNoticiaCanviBinding
 
     private val myAdapter: NoticiaRecyclerAdapter = NoticiaRecyclerAdapter()
+
+    //Declarem i incialitzem un atribut de tipus FirebaseFirestore, classe on trobarem els mètodes per treballar amb la base de dades Firestore
+    private var bd =
+        FirebaseFirestore.getInstance() //Inicialitzem mitjançant el mètode getInstance() de FirebaseFirestore
 
     private fun setupRecyclerView() {
         binding.recyclerNoticies.setHasFixedSize(true)
@@ -37,7 +51,7 @@ class noticia_canvi : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val btnAddNot = requireView().findViewById<Button>(R.id.botoAfegirNoticia)
-
+        mostrarNoticies()
         btnAddNot.setOnClickListener{
             findNavController().navigate(R.id.action_noticia_fragment_to_afegirNoticia)
         }
@@ -53,16 +67,32 @@ class noticia_canvi : Fragment() {
 
     private fun getNoticies():MutableList<NoticiaModel>{
         val noticies: MutableList<NoticiaModel> = arrayListOf()
-        noticies.add(NoticiaModel("Noticia important", "Aquesta noticia es molt important!!!", "09/07/2021"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-        noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
 
         return noticies
+    }
 
+    private fun mostrarNoticies() {
+
+        lifecycleScope.launch {
+            bd.collection("Noticies").get().addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val wallItem = NoticiaModel(
+                        title = document["titolNoticia"].toString(),
+                        content = document["contingutNoticia"].toString(),
+                        date = document["dateNoticia"].toString()
+                    )
+                    if (getNoticies().isEmpty()) {
+                        getNoticies().add(wallItem)
+                    } else {
+                        for (i in getNoticies()) {
+                            if (wallItem.titolNoticia != i.titolNoticia) {
+                                getNoticies().add(wallItem)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
