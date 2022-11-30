62f708fbaaeb4f1014aab05fd9e115e752da8051
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2022-11-30 09:14:30 +0100

    classe Utilities

--- app/src/main/java/cat/copernic/bergants/Utilities.kt
+++ app/src/main/java/cat/copernic/bergants/Utilities.kt
@@ -5,32 +5,34 @@ import android.widget.ProgressBar
 import com.google.android.material.snackbar.Snackbar
 import java.util.regex.Pattern
 
-fun campEsBuit(correu:String,contrasenya:String):Boolean{
-        return correu.isNotEmpty()&&contrasenya.isNotEmpty()
+class Utilities {
+    fun campEsBuit(correu: String, contrasenya: String): Boolean {
+        return correu.isNotEmpty() && contrasenya.isNotEmpty()
     }
 
-fun View.snack(message: String) {
-    Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
-}
+    fun View.snack(message: String) {
+        Snackbar.make(this, message, Snackbar.LENGTH_LONG).show()
+    }
 
-var email_Param =
-    Pattern.compile("^[_a-z0-9]+(.[_a-z0-9]+)*@[a-z0-9]+(.[a-z0-9]+)*(.[a-z]{2,4})\$")
+    var email_Param =
+        Pattern.compile("^[_a-z0-9]+(.[_a-z0-9]+)*@[a-z0-9]+(.[a-z0-9]+)*(.[a-z]{2,4})\$")
 
-fun isValidEmail(email: CharSequence?): Boolean {
-    return if (email == null) false else email_Param.matcher(email).matches()
-}
+    fun isValidEmail(email: CharSequence?): Boolean {
+        return if (email == null) false else email_Param.matcher(email).matches()
+    }
 
-//Funcions de la barra de progrés
-private var progressBar: ProgressBar? = null
+    //Funcions de la barra de progrés
+    private var progressBar: ProgressBar? = null
 
-fun setProgressBar(bar: ProgressBar) {
-    progressBar = bar
-}
+    fun setProgressBar(bar: ProgressBar) {
+        progressBar = bar
+    }
 
-fun showProgressBar() {
-    progressBar!!.visibility = View.VISIBLE
-}
+    fun showProgressBar() {
+        progressBar!!.visibility = View.VISIBLE
+    }
 
-fun hideProgressBar() {
-    progressBar!!.visibility = View.INVISIBLE
+    fun hideProgressBar() {
+        progressBar!!.visibility = View.INVISIBLE
+    }
 }
