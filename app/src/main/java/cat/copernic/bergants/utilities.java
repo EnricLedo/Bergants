e6c50b8223155bfa3bdb165d702cd001a02a4981
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-11-30 09:07:16 +0100

    Classe utilities

---
+++ app/src/main/java/cat/copernic/bergants/utilities.java
@@ -0,0 +1,7 @@
+package cat.copernic.bergants;
+
+public class utilities {
+    fun campEsBuit(correu:String,contrasenya:String):Boolean{
+        return correu.isNotEmpty()&&contrasenya.isNotEmpty()
+    }
+}
