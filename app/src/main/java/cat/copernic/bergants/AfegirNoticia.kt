622ce021ff27c4a9aca76d92b7c0fe0685947225
= <=>
2022-11-30 09:27:24 +0100

    Petit ajust de la splash screen

--- app/src/main/java/cat/copernic/bergants/AfegirNoticia.kt
+++ app/src/main/java/cat/copernic/bergants/AfegirNoticia.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import cat.copernic.bergants.databinding.FragmentAfegirNoticiaBinding
 import cat.copernic.bergants.model.NoticiaModel
