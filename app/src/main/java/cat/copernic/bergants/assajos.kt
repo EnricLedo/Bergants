8fdc6db39b59b9f27236059733fa9a5e5b30197a
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-12-12 23:09:03 +0100

    Informació als recycler views d'assajos funciona (crec)

--- app/src/main/java/cat/copernic/bergants/assajos.kt
+++ app/src/main/java/cat/copernic/bergants/assajos.kt
@@ -14,11 +14,15 @@ import cat.copernic.bergants.databinding.FragmentAssajosBinding
 import cat.copernic.bergants.model.ActuacioModel
 import cat.copernic.bergants.model.AssaigModel
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class assajos : Fragment() {
 
     private lateinit var binding: FragmentAssajosBinding
+
+    private var list_multable: MutableList<AssaigModel> = ArrayList()
 
     private val myAdapter: AssaigRecyclerAdapter = AssaigRecyclerAdapter()
 
@@ -26,7 +30,7 @@ class assajos : Fragment() {
         FirebaseFirestore.getInstance() //Inicialitzem mitjançant el mètode getInstance() de FirebaseFirestore
 
     private fun setupRecyclerView() {
-        if (getAssajos().isEmpty()) {
+        if (list_multable.isEmpty()) {
             mostrarAssajos()
         } else {
             binding.recyclerAssajos.setHasFixedSize(true)
@@ -35,7 +39,7 @@ class assajos : Fragment() {
             binding.recyclerAssajos.layoutManager = LinearLayoutManager(context)
 
             //generem el adapter
-            myAdapter.AssaigRecyclerAdapter(getAssajos(), requireActivity())
+            myAdapter.AssaigRecyclerAdapter(list_multable, requireActivity())
             //assignem el adapter al RV
             binding.recyclerAssajos.adapter = myAdapter
         }
@@ -96,8 +100,41 @@ class assajos : Fragment() {
     }
 
     private fun mostrarAssajos() {
-
         lifecycleScope.launch {
+            withContext(Dispatchers.IO){
+                bd.collection("Assajos").get().addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val wallItem = AssaigModel(
+                            title = document["titolAssaig"].toString(),
+                            data = document["dataAssaig"].toString(),
+                            lloc = document["llocAssaig"].toString()
+                        )
+                        if (list_multable.isEmpty()) {
+                            list_multable.add(wallItem)
+                        } else {
+                            var contador = 0
+                            for (i in list_multable) {
+                                if (wallItem.title == i.title) {
+                                    contador++
+                                }
+                            }
+                            if(contador <1){
+                                list_multable.add(wallItem)
+                            }
+                        }
+                    }
+                    //indiquem que el RV es mostrarà en format llista
+                    binding.recyclerAssajos.layoutManager = LinearLayoutManager(context)
+
+                    //generem el adapter
+                    myAdapter.AssaigRecyclerAdapter(list_multable, requireActivity())
+                    //assignem el adapter al RV
+                    binding.recyclerAssajos.adapter = myAdapter
+                }
+            }
+        }
+
+        /**lifecycleScope.launch {
             bd.collection("Assajos").get().addOnSuccessListener { documents ->
                 for (document in documents) {
                     val wallItem = AssaigModel(
@@ -123,6 +160,6 @@ class assajos : Fragment() {
                 //assignem el adapter al RV
                 binding.recyclerAssajos.adapter = myAdapter
             }
-        }
+        }*/
     }
 }
