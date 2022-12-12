00bc579cb015b3edb43bf8315d088d0bfa3528fd
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2022-12-12 09:05:11 +0100

    Llegir dades noticies (NO FUNCIONAAAA)

--- app/src/main/java/cat/copernic/bergants/noticia_canvi.kt
+++ app/src/main/java/cat/copernic/bergants/noticia_canvi.kt
@@ -80,7 +80,6 @@ class noticia_canvi : Fragment() {
 
     private fun getNoticies(): MutableList<NoticiaModel> {
         val noticies: MutableList<NoticiaModel> = arrayListOf()
-
         noticies.add(NoticiaModel("Noticia important", "Aquesta noticia es molt important!!!", "09/07/2021"))
         noticies.add(NoticiaModel("Anem d'excursió", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
         noticies.add(NoticiaModel("Anem d'excursió a casa del Marc", "Recordeu que avui anirem al concert d'Adri Navarro. Porteu-vos: el merch de Cucurella i moltes ganes de passar-ho bé.", "12-10-22 19:37h"))
@@ -88,9 +87,7 @@ class noticia_canvi : Fragment() {
         noticies.add(NoticiaModel("Anem d'excursió a casa del Joan", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
         noticies.add(NoticiaModel("No anem d'excursió", "Recordeu que avui no anirem d'excursió a Montserrat. No porteu: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
         noticies.add(NoticiaModel("Anem d'excursió a algun lloc", "Recordeu que avui anirem d'excursió a Montserrat. Porteu-vos: motxilla, cantimplora, entrepà, botes de muntanya i moltes ganes de passar-ho bé.", "27-10-22 9:27h"))
-
         return noticies
-
     }
 
     private fun mostrarNoticies() {
