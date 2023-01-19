38922ddc086a909b442dc2313dd9f6c6fa5aef92
=Marc Fernández <=fernandez.gonzalez.marc@alumnat.copernic.cat>
2023-01-19 13:06:58 +0100

    Validar k el correu és valid a afegirMembre.kt

--- app/src/main/java/cat/copernic/bergants/afegirMembre.kt
+++ app/src/main/java/cat/copernic/bergants/afegirMembre.kt
@@ -191,13 +191,6 @@ class afegirMembre : Fragment() {
             // i el mostra. A continuació, crida al mètode de notificació amb els valors de "titolAssaig"
             // i "llocAssaig" com a arguments. Si la tasca no té èxit, crea un AlertDialog amb un missatge
             // "assaigWrong" que té un sol botó "Acceptar" i el mostra.
-            .addOnSuccessListener {
-                val builder = AlertDialog.Builder(requireContext())
-                builder.setMessage(getString(R.string.membreCorredct))
-                builder.setPositiveButton(getString(R.string.aceptar), null)
-                val dialog = builder.create()
-                dialog.show()
-            }
             .addOnFailureListener {
                 val builder = AlertDialog.Builder(requireContext())
                 builder.setMessage(getString(R.string.membreWrong))
@@ -263,6 +256,23 @@ class afegirMembre : Fragment() {
             val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
             if (correuMembre.matches(Regex(emailRegex))&&passwordOkMembre.isNotEmpty()){
                 registrar(correuMembre, passwordOkMembre,)
+                var membre = llegirDades()
+                if (membre.nomMembre?.isNotEmpty() == true && membre.malnom?.isNotEmpty() == true && membre.alcadaEspatlles?.isNotEmpty() == true
+                    && membre.alcadaMans?.isNotEmpty() == true && membre.correuMembre?.isNotEmpty() == true
+                    && membre.adrecaMembre?.isNotEmpty() == true && membre.telefonMembre?.isNotEmpty() == true
+                    && membre.rolMembre?.isNotEmpty() == true && membre.altaMembre?.isNotEmpty() == true) {
+                    afegirMembre(membre)
+                    findNavController().navigate(R.id.action_afegirMembre_to_membres_fragment)
+
+                    val builder = AlertDialog.Builder(requireContext())
+                    builder.setMessage(getString(R.string.membreCorredct))
+                    builder.setPositiveButton(getString(R.string.aceptar), null)
+                    val dialog = builder.create()
+                    dialog.show()
+
+                } else {
+                    Snackbar.make(it, getString(R.string.parametres), Snackbar.LENGTH_LONG).show()
+                }
             }else {
                 Snackbar.make(it, getString(R.string.correuMal), Snackbar.LENGTH_LONG).show()
             }
@@ -274,6 +284,17 @@ class afegirMembre : Fragment() {
                 && membre.rolMembre?.isNotEmpty() == true && membre.altaMembre?.isNotEmpty() == true) {
                 afegirMembre(membre)
                 findNavController().navigate(R.id.action_afegirMembre_to_membres_fragment)
+                if (correuMembre.matches(Regex(emailRegex))&&passwordOkMembre.isNotEmpty()){
+                    registrar(correuMembre, passwordOkMembre,)
+
+                    val builder = AlertDialog.Builder(requireContext())
+                    builder.setMessage(getString(R.string.membreCorredct))
+                    builder.setPositiveButton(getString(R.string.aceptar), null)
+                    val dialog = builder.create()
+                    dialog.show()
+                }else {
+                    Snackbar.make(it, getString(R.string.correuMal), Snackbar.LENGTH_LONG).show()
+                }
 
             } else {
                 Snackbar.make(it, getString(R.string.parametres), Snackbar.LENGTH_LONG).show()
